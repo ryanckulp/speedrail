02f955cfc5a18663ccf40986f7f5f43c8084523c
Ryan Kulp <ryanckulp@gmail.com>
2019-02-26 21:09:32 +0800

    simple test logging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.log_level = :fatal # none
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
