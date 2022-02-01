74923bdf071710962b193ee2ab6ec7e4cb7ebab4
Ryan Kulp <ryanckulp@gmail.com>
2022-02-01 22:25:23 +0900

    setup color palette

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'primary': '#DA5827',
+        'primary-hover-700': {
+          '700': '#a4411c', // darkened primary 25% https://pinetools.com/darken-color
+          '500': '#EA9A72'
+        }
+      }
     },
   },
   plugins: [
