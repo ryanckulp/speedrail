c68cccb750993cb1737b114d5e4e93763315282c
Ryan Kulp <ryanckulp@gmail.com>
2022-02-01 14:50:25 +0900

    devise - user signup/login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+		dashboard_index_path # point this wherever you want
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   devise_for :users
+
+  # devise_scope :user do
+  #   get 'signup', to: 'devise/registrations#new'
+  #   get 'login', to: 'devise/sessions#new'
+  # end
+
+  resources :dashboard, only: [:index]
+
   get 'logout', to: 'pages#logout', as: 'logout'
 
   # static pages
