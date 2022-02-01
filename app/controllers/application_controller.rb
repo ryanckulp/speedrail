1e7641aeae2ac2671b54b569e9d813a458ae6a8a
Ryan Kulp <ryanckulp@gmail.com>
2022-02-01 20:08:59 +0900

    cleanup app wide routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-
   def after_sign_in_path_for(resource)
-		dashboard_index_path # point this wherever you want
+    resource.paying_customer? ? dashboard_index_path : subscribe_index_path # point these wherever you want
 	end
+
+  def maybe_skip_onboarding
+    redirect_to dashboard_index_path, notice: "You're already subscribed" if current_user.try(:subscribed?)
+  end
 end
