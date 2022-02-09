ba23e5faaefc8b65ba10de0274f44315dafe3380
Ryan Kulp <ryanckulp@gmail.com>
2022-02-09 21:22:38 +0900

    better color setup

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
       },
       colors: { // custom color palette for branding
         'primary': 'rgb(79, 70, 229)',
-        'primary-hover-700': {
-          '700': '#a4411c', // use https://pinetools.com/darken-color
+        'primary-hover': {
+          '700': '#a4411c',
           '500': '#EA9A72'
         }
       }
