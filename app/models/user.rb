31e760808b092826d3df9f280a828f3823331b33
Ryan Kulp <ryanckulp@gmail.com>
2022-02-14 00:30:48 +0900

    user validation for specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
 
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+
   scope :subscribed, -> { where(paying_customer: true) }
 
   after_create do
