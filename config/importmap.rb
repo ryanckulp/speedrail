c1ed286b811fdada6dfdf19f0af48bbe83d8d44e
Ryan Kulp <ryanckulp@gmail.com>
2023-05-10 18:17:27 -0400

    better support for action cable

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
