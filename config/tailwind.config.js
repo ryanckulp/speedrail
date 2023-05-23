c2bb11484335d932e8aef9f4c766e1debb9642f4
Ryan Kulp <ryanckulp@gmail.com>
2023-05-23 19:14:46 -0400

    misc cleanup

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       },
       colors: { // custom color palette for branding
         'primary': 'rgb(79, 70, 229)',
