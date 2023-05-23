e83e4633e1aefe19e182e08ffff5ba118f2f1926
Ryan Kulp <ryanckulp@gmail.com>
2023-05-23 19:14:07 -0400

    stub to handle custom font files with example

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../fonts
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
