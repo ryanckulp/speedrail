bbc9624b3edc1b1ce1731a9144b6def2fb8c2fd2
Ryan Kulp <ryanckulp@gmail.com>
2023-09-29 09:44:11 -0400

    option to whitelist additional User params during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # uncomment to allow extra User model params during registration (beyond email/password)
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     resource.paying_customer? ? dashboard_index_path : subscribe_index_path # point these wherever you want
 	end
@@ -6,4 +9,12 @@ class ApplicationController < ActionController::Base
   def maybe_skip_onboarding
     redirect_to dashboard_index_path, notice: "You're already subscribed" if current_user.finished_onboarding?
   end
+
+
+  protected
+
+  # whitelist extra User model params by uncommenting below and adding User attrs as keys
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  # end
 end
