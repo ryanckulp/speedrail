aeb894aedd6a36c30aa5fdac180ddae25643118c
Ryan Kulp <ryanckulp@gmail.com>
2023-10-03 14:42:28 -0400

    ENV-ify client id

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,6 +1,6 @@
 class OauthController < ApplicationController
   def authorize
-    redirect_to "https://app.hubspot.com/oauth/authorize?client_id=0d6463f1-43cf-4156-9aa8-aa14e52020a2&redirect_uri=#{ENV['HUBSPOT_REDIRECT_URI']}&scope=#{ENV['HUBSPOT_SCOPES']}", allow_other_host: true
+    redirect_to "https://app.hubspot.com/oauth/authorize?client_id=#{ENV['HUBSPOT_CLIENT_ID']}&redirect_uri=#{ENV['HUBSPOT_REDIRECT_URI']}&scope=#{ENV['HUBSPOT_SCOPES']}", allow_other_host: true
   end
 
   def callback
