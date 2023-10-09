eeb222b53dbe54e9f595d0b1d61eb444a027e249
Ryan Kulp <ryanckulp@gmail.com>
2023-10-09 14:58:59 -0400

    add back Speedrail defaults (allow ngrok host, letter_opener in dev, CORS default, generators callbacks, queue adapter, mailers

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,35 @@ module Speedrail
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    # allow cross origin requests
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+      end
+    end
+
+    # background jobs
+    config.active_job.queue_adapter = :delayed
+
+    # mailers via postmark
+    config.action_mailer.default_url_options = { host: ENV['BASE_URL'] }
+    config.action_mailer.default_options = { from: ENV['ADMIN_EMAIL'] }
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
+
+    # serve images from asset pipeline in mailers
+    config.asset_host = ENV['BASE_URL']
+
+    # customize generators
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.view_specs = false
+      g.helper_specs = false
+      g.assets = false # stylesheets
+      g.helper = true
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,14 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # allow requests as dynamic domains, e.g. ngrok.io, ngrok.app
+  config.hosts << /[a-z0-9-]+\.ngrok..*/
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+
+  # open mailers in the browser
+  config.action_mailer.delivery_method = :letter_opener
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
