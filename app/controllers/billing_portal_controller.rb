1dea354e678167dbf6c1ae9420d069155d59f419
Ryan Kulp <ryanckulp@gmail.com>
2023-10-26 11:29:19 -0700

    refactor billing controller to embed stripe checkout and redirect to "listener" interstitial for status check

--- app/controllers/billing_portal_controller.rb
+++ app/controllers/billing_portal_controller.rb
@@ -2,44 +2,53 @@ class BillingPortalController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, only: [:create] # ajax
 
-  def create
-    url = current_user.finished_onboarding? ? modify_subscription : begin_subscription
+  def new
+    session = Stripe::Checkout::Session.retrieve(params[:session_id])
 
-    respond_to do |format|
-      format.html { redirect_to url, allow_other_host: true }
-      format.json { render json: { url: url } }
+    # can add more checks, just inspect the 'session' and 'session.customer_details' object
+    if session.status == 'complete'
+      current_user.delay.set_stripe_subscription
+      redirect_to dashboard_index_path, notice: 'Your account is now active!'
+    else
+      redirect_to subscribe_index_path, alert: "Please subscribe to continue."
     end
   end
 
-  def destroy
-    current_user.unsubscribe!
+  def create
+    if current_user.finished_onboarding?
+      redirect_to modify_subscription, allow_other_host: true
+    else
+      render json: create_subscription
+    end
   end
 
   private
 
   # invoked from /subscribe during onboarding
-  def begin_subscription
+  def create_subscription
     session = Stripe::Checkout::Session.create({
+      ui_mode: 'embedded',
       customer: current_user.stripe_customer_id,
-      payment_method_types: ['card'],
-      mode: 'subscription',
       allow_promotion_codes: true,
+      payment_method_types: ['card'],
       line_items: [{
-        quantity: 1,
-        price: ENV['STRIPE_PRODUCT_PRICE_ID']
+        # Provide the Price ID (e.g. price_1234) of the product you want to sell
+        price: ENV['STRIPE_PRODUCT_PRICE_ID'],
+        quantity: 1
       }],
-      success_url: "#{ENV['BASE_URL']}#{dashboard_index_path}?subscribed=true",
-      cancel_url: "#{ENV['BASE_URL']}#{account_index_path}?aborted=true"
+      mode: 'subscription', # use 'payment' for products with 1-time pricing
+      return_url: "#{ENV['BASE_URL']}#{new_billing_portal_path}?session_id={CHECKOUT_SESSION_ID}",
+      automatic_tax: { enabled: false }
     })
 
-    session.url
+    { clientSecret: session.client_secret }
   end
 
   # invoked from /account when user is already subscribed
   def modify_subscription
     session = Stripe::BillingPortal::Session.create({
       customer: current_user.stripe_customer_id,
-      return_url: "#{ENV['BASE_URL']}/#{account_index_path}?updated=true"
+      return_url: "#{ENV['BASE_URL']}#{account_index_path}?updated=true"
     })
 
     session.url
