606220ce5d9a29c07b7463b4e5b5d6e05f9cf141
5andu <sandugolovatenco@gmail.com>
2023-10-27 17:10:26 +0700

    auto remove flash notices and errors

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,10 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+// Used to remove notifications after a few seconds
+import RemovalsController from "./removals_controller.js";
+application.register("removals", RemovalsController);
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
