d6ed5d052aaf03a5bec59f0dd7db7f9c3df668d7
5andu <sandugolovatenco@gmail.com>
2024-01-10 21:27:17 +0100

    Refactor and simplify trix editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -22,7 +22,7 @@ addEventListener("trix-initialize", (event) => {
   h1Button.insertAdjacentHTML(
     "afterend",
     `
-    <button type="button" class="trix-button trix-button--icon trix-button--icon-heading-2 flex justify-center" data-trix-attribute="heading2" title="Heading 2" tabindex="-1" data-trix-active=""><svg class="p-0.5" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 4V20M18 4V20M8 4H4M18 12L6 12M8 20H4M20 20H16M20 4H16" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-heading-2 flex justify-center" data-trix-attribute="heading2" title="Heading 2" tabindex="-1" data-trix-active="">
     </button>
   `
   );
@@ -41,7 +41,7 @@ addEventListener("trix-initialize", (event) => {
   h2Button.insertAdjacentHTML(
     "afterend",
     `
-    <button type="button" class="trix-button trix-button--icon trix-button--icon-heading-3 flex justify-center" data-trix-attribute="heading3" title="Heading 3" tabindex="-2" data-trix-active=""><svg class="p-0.5" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 4V20M18 4V20M9.5 4V20M11.5 4H4M18 12H9.5M11.5 20H4M20 20H16M20 4H16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-heading-3 flex justify-center" data-trix-attribute="heading3" title="Heading 3" tabindex="-2" data-trix-active="">
     </button>
   `
   );
