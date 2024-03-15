85177211f3a7767839bec5e33e18c3cb126042f4
Ryan Kulp <ryanckulp@gmail.com>
2024-03-14 22:32:55 -0400

    blog post tests

---
+++ spec/factories/blog_posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :blog_post do
+    title { 'Ryan is awesome' }
+    slug { 'kulp-equals-chad' }
+    description { 'nothing more required' }
+    body { 'we make our own luck' }
+  end
+end
---
+++ spec/models/blog_post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe BlogPost, type: :model do
+  subject { build(:blog_post) }
+
+  it 'has a valid factory' do
+    expect(subject).to be_valid
+  end
+
+  describe 'ActiveModel validations' do
+    it { expect(subject).to validate_presence_of(:title) }
+    it { expect(subject).to validate_presence_of(:description) }
+    it { expect(subject).to validate_presence_of(:body) }
+  end
+
+  describe 'slug generator' do
+    it 'works with proper input data' do
+      post = create(:blog_post, slug: 'basic-slug')
+      expect(post.slug).to eql 'basic-slug'
+    end
+
+    it 'works with improper input data' do
+      post = create(:blog_post, slug: 'Complex     slug HERE')
+      expect(post.slug).to eql 'complex-slug-here'
+    end
+
+    it 'works when slug already exists' do
+      create(:blog_post, slug: 'unique-slug')
+      post = create(:blog_post, slug: 'unique-slug')
+      expect(post.slug).to_not eql 'unique-slug'
+      expect(post.slug).to include('unique-slug-')
+    end
+  end
+end
