51164781d519e6d46aab617c5151c3b1e7a4a735
Ryan Kulp <ryanckulp@gmail.com>
2024-04-08 11:53:03 -0400

    setup Split, sample A/B test, and admin panels

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu priority: 2, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "px-4 py-16 md:py-32 text-center m-auto max-w-3xl" do
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -206,11 +206,12 @@ ActiveAdmin.setup do |config|
   #
   # If you wanted to add a static menu item to the default menu provided:
 
-  # config.namespace :admin do |admin|
-  #   admin.build_menu :default do |menu|
-  #     menu.add label: 'Sidekiq', url: '/admin/sidekiq', html_options: { target: :blank }
-  #   end
-  # end
+  config.namespace :admin do |admin|
+    admin.build_menu :default do |menu|
+      menu.add label: '← Back to app', url: '/', priority: 1
+      menu.add label: 'A/B Testing', url: '/admin/split', html_options: { target: :blank }
+    end
+  end
 
   # == Download Links
   #
